1be04e33734d50d7cad731b6dfb4b97bf6b19617
Krzysztof_R <romanowski.krzysiu@gmail.com>
2017-11-12 20:16:33 +0100

    Wygenerowanie pytan w pliku XML

--- Gui/ConsoleApp/Program.cs
+++ Gui/ConsoleApp/Program.cs
@@ -13,7 +13,18 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var ws = new WebService(8001); 
+            // var ws = new WebService(8001);
+
+            StorageService ss = new StorageService();
+
+            QuestionsSet qs = ss.ParseTextFile(
+                @"C:\Users\Krzysztof\Desktop\dane\Kod\Git\AON-PROJECT\Gui\AwanturaTests\TestsData\pytania.tsv",
+                QuestionsSet.TextFormatParser);
+
+            ss.SerializeToXMLFile(qs, 
+                @"C:\Users\Krzysztof\Desktop\dane\Kod\Git\AON-PROJECT\Gui\AwanturaTests\TestsData\pytania.xml");
+
+            Console.ReadKey();
         }
     }
 }
