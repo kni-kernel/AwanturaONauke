59daa76cd472e7b80678a9e60b460adc98b85c18
Vectrom <damian.plociennik@mybboard.pl>
2017-11-13 16:47:33 +0100

    srutututu

--- Gui/AwanturaLib/MainService.cs
+++ Gui/AwanturaLib/MainService.cs
@@ -55,7 +55,7 @@ namespace AwanturaLib
 
         public GameState Bet(GameState gamestate, int index, int amount)
         {
-            if(amount > 0 && gamestate.Licitation.Bid.Max() < amount && gamestate.Teams[index].Points > 300)
+            if(gamestate.State == States.Licitation && amount > 0 && gamestate.Licitation.Bid.Max() < amount && gamestate.Teams[index].Points > 300)
                 gamestate.Licitation.bet(gamestate, index, amount);
 
             return gamestate;
@@ -63,7 +63,7 @@ namespace AwanturaLib
 
         public GameState BetWithoutRestrictions(GameState gamestate, int index, int amount)
         {
-            if(amount > 0 && gamestate.Teams[index].Points > 300)
+            if(gamestate.State == States.Licitation && amount > 0 && gamestate.Teams[index].Points > 300)
                 gamestate.Licitation.bet(gamestate, index, amount);
 
             return gamestate;
